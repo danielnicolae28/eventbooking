865df566ef9758a5c4219323778789bc7b9e89a0
danielnicolae28 <danielnicolae286@gmail.com>
2024-08-10 16:19:24 +0300

    post and get basic methods

--- main.go
+++ main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/danielnicolae28/eventbooking/models"
+	"github.com/gin-gonic/gin"
+)
+
+/*with gin*/
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // local host
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		return
+
+	}
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
+}
